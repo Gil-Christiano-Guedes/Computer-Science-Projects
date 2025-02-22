69f9751d347257445f5ae73ba1d5d6efaaf44ee3
Gil Christiano Guedes dos Santos <gilchristiano@gmail.com>
2025-02-22 17:18:27 -0300

    Atualizações (ver 1.5)

--- eclipse-workspace/Cursos/src/Curso/Operacoes_com_Strings.java
+++ eclipse-workspace/Cursos/src/Curso/Operacoes_com_Strings.java
@@ -1,10 +1,21 @@
 package Curso;
+import java.util.Scanner;
 
 public class Operacoes_com_Strings {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		String userName;
+		try (Scanner gravador = new Scanner(System.in)) {
+			System.out.print("Qual é seu nome: ");
+			userName = gravador.nextLine();
+		}
+		
+		Account conta;
+		conta = new Account();
+		conta.setName(userName);
+		System.out.printf("Nome: %s %s", conta.getName(), " | Deu certo! | ");
+		// %S retorna strings em caixa alta
+		System.out.printf("\nNome: %S ", conta.getName());	
 	}
-
 }
---
+++ eclipse-workspace/Cursos/src/classe_string/Account.java
@@ -0,0 +1,14 @@
+package classe_string;
+
+public class Account {
+	
+	private String name;
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+}
